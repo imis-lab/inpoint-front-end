481a2a05a344dd50be80843bafa5aae1676b3d42
Nikos Kanakaris <nikos.kanakaris89@gmail.com>
2021-02-20 18:27:28 +0200

    Add a basic discourse graph component

---
+++ src/components/discourceGraph.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class DiscourceGraph extends Component {
+    state = {  }
+    render() { 
+        return (<h1>this is the disource graph</h1>);
+    }
+}
+ 
+export default DiscourceGraph;
