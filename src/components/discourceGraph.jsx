a1712ff8dee8ab445392f83a832956a6551fed1a
Nikos Kanakaris <nikos.kanakaris89@gmail.com>
2021-02-20 18:47:53 +0200

    Add a basic graph

--- src/components/discourceGraph.jsx
+++ src/components/discourceGraph.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import CytoscapeComponent from "react-cytoscapejs";
 
 class DiscourceGraph extends Component {
-    state = {  }
-    render() { 
-        return (<h1>this is the disource graph</h1>);
-    }
+    constructor(props){
+        super(props);
+      }
+
+      render(){
+        const elements = [
+           { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+           { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+           { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+        ];
+    
+        return (<CytoscapeComponent elements={elements} style={ { width: '1500px', height: '600px' } } />);
+      }
 }
  
 export default DiscourceGraph;
